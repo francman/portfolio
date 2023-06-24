4fdfe763382552d2688f4b21be51570cb7ca4eee
francman <frankmanu500@gmail.com>
2023-06-24 16:14:06 -0400

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,17 +37,15 @@ const Hero = () => {
           <div>
             <h1
               className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}>
-              Hi, I'm{' '}
-              <span
-                className="sm:text-battleGray sm:text-[90px] 
-                text-eerieBlack text-[50px] font-mova
+              Hello, I&#39;m{' '}
+              <span className="sm:text-battleGray sm:text-[90px] 
+                text-eerieBlack text-[30px] font-mova
                 font-extrabold uppercase">
-                Shaquille
+                Frank
               </span>
             </h1>
-            <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
-              Lorem ipsum dolor sit amet. <br className="sm:block hidden" />
-              consectetur adipisicing elit deleniti, voluptas.
+            <p className={`${styles.heroSubText} mt-2`}>
+              I do Systems Engineering
             </p>
           </div>
           <div
@@ -55,29 +53,6 @@ const Hero = () => {
             justify-center sm:-ml-[3rem] xxs:mt-4"></div>
 
           <div></div>
-        </div>
-
-        <div
-          className="absolute xs:bottom-10 bottom-32 w-full 
-          flex justify-center items-center">
-          <a href="#about">
-            <div
-              className="w-[35px] h-[64px] rounded-3xl border-4 
-            border-french border-dim flex
-            justify-center items-start p-2">
-              <motion.div
-                animate={{
-                  y: [0, 24, 0],
-                }}
-                transition={{
-                  duration: 1.5,
-                  repeat: Infinity,
-                  repeatType: 'loop',
-                }}
-                className="w-3 h-3 rounded-full bg-taupe mb-1"
-              />
-            </div>
-          </a>
         </div>
 
         {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
